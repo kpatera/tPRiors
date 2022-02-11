1611f0a109c7974b4188aac3c5bfacde3a568446
kostas.pateras@gmail.com <63882794+kpateras@users.noreply.github.com>
2022-02-11 10:02:59 +0200

    Add instructions

--- global.R
+++ global.R
@@ -20,10 +20,10 @@ fb<-list(1,1)
 
 #---- Load pre-loaded datasets ----#
 require(readxl)
-Example1_4Studies <<- data.frame(read_excel("Data/efs31678e-sup-0004-annex_d_4studies.xlsx"))
-Example2_40Studies <<- data.frame(read_excel("Data/efs31678e-sup-0004-annex_d_Attica.xlsx"))
+ElisaData_4Studies <<- data.frame(read_excel("Data/efs31678e-sup-0004-annex_d_4studies.xlsx"))
+ElisaData_40Studies <<- data.frame(read_excel("Data/efs31678e-sup-0004-annex_d_Attica.xlsx"))
 #Example3_102Studies <<- data.frame(read_excel("Data/efs31678e-sup-0004-annex_d_NonAttica.xlsx"))
-Example4_129studies <<- data.frame(read_excel("Data/efs31678e-sup-0004-annex_d.xlsx"))
+ElisaData_129studies <<- data.frame(read_excel("Data/efs31678e-sup-0004-annex_d.xlsx"))
 Dementia_Motivating <<- data.frame(read_excel("Data/Dementia_Updated.xls"))
 
 name_data<-"sidebar user input"
